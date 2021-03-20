7327750f5695dc6855d6288ee80137a9fc106746
raniafathallah <raniaahmed7000@gmail.com>
2021-03-20 03:33:07 +0200

    .

---
+++ Reporting/FRM_RPT_PRODUCT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductsApp.Reporting
+{
+    public partial class FRM_RPT_PRODUCT : Form
+    {
+        public FRM_RPT_PRODUCT()
+        {
+            InitializeComponent();
+        }
+
+        private void FRM_RPT_PRODUCT_Load(object sender, EventArgs e)
+        {
+
+     
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
